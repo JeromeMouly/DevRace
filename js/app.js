150896de13cdf8ecfc11727bf8bded732d3f62a8
JeromeMouly <mrsyljay@gmail.com>
2017-10-31 15:16:22 +0100

    Ajout pages team, partners et modal

--- js/app.js
+++ js/app.js
@@ -7,3 +7,21 @@ function openMenu() {
 function closeMenu() {
     document.getElementById("menu").style.width = "0%";
 }
+
+
+/* ----- Back to top Btn ----- */
+
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
